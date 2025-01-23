334aafc8cc1f323820e31da6c757d97b600a65c5
SenyuMei <SenYuMei@hotmail.com>
2025-01-23 20:08:52 +0100

    Create LoPrincipalDeJava.java

---
+++ LoPrincipalDeJava.java
@@ -0,0 +1,16 @@
+Crear fichero de texto java:
+
+    - touch nombre_del_fichero.java
+    
+Compilar fichero de texto java:
+
+    - javac nombre_del_fichero.java
+
+    + Siempre los ficheros acaban por .java salvo a la hora de ejecutar
+  
+Ejecutar fichero de texto java:
+
+    - java nombre_del_fichero
+
+  
+
