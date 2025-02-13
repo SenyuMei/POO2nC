a863cc57ca93f6dbce03398c33decb479f972adf
SenyuMei <SenYuMei@hotmail.com>
2025-02-13 10:46:40 +0100

    Create Numeros.java

---
+++ Numeros.java
@@ -0,0 +1,8 @@
+console.log("Para imprimir números en la consola, se usa console.log pero dentro no lleva comillas")
+console.log(420)
+// en caso de que llevara comillas ej. console.log("420") el programa lo tomara como un string
+// y no como un número o en otras palabras un integer
+
+//output:
+// Para imprimir números en la consola, se usa console.log pero dentro no lleva comillas
+// 420
