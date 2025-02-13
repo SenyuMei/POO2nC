0e62b21e5b6af3dd16e321ed81768904e807e21e
SenyuMei <SenYuMei@hotmail.com>
2025-02-13 12:57:04 +0100

    Create Prompt.java

---
+++ Prompt.java
@@ -0,0 +1,9 @@
+const nombre = prompt("Introduce tu nombre: ")
+// lee el string del usuario
+
+console.log("Hola", nombre);
+
+const edad = prompt("Introduce tu edad: ")
+console.log(typeof(edad));
+// output: string 
+// los númerops tambien estan dados como un string
