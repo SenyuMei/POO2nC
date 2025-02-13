7a89f48c8fe6c1a2d29bc7d8f835595327c94c7e
SenyuMei <SenYuMei@hotmail.com>
2025-02-13 12:42:20 +0100

    Create VariabeString.java

---
+++ VariabeString.java
@@ -0,0 +1,12 @@
+let ciu = "Barcelona";
+console.log("Ciudad:" +ciu);
+
+// lo que ca entre comillas es un string y +variable imprime la variable asignada
+
+let sup = 149;
+let nombre = "DIA";
+
+console.log("En " + ciu + " hay " + sup + " supermercados " +nombre);
+
+console.log(`En ${ciu} hay ${sup} supermercados ${nombre}`);
+// otra forma de escribirlo es esa pero con ``
