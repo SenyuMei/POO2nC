3d2eb65b7fe3fab8c3c49b12b4de49fa2043b923
SenyuMei <SenYuMei@hotmail.com>
2025-02-13 11:46:35 +0100

    Update Constante.java

--- Constante.java
+++ Constante.java
@@ -1,1 +1,7 @@
+// const es una asignacion para crear una variable que no cambia 
+const pi = 3.14;
+console.log(pi);
 
+pi = 123;
+console.log(pi);
+// da un error ya que a una constante no se le puede asignar otro valor
