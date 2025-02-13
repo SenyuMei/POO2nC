ef39fbd53856bf2d3d5e618c4cbe518380a35ca4
SenyuMei <SenYuMei@hotmail.com>
2025-02-13 13:00:47 +0100

    Create Value.java

---
+++ Value.java
@@ -0,0 +1,14 @@
+const intValue = 5;
+const floatValue = 5.5;
+
+console.log(intValue)
+console.log(floatValue)
+
+console.log(typeof(intValue))
+console.log(typeof(floatValue))
+
+// output:
+// 5
+// 5.5
+// number
+// number      ambos tanto sea un float como un int para el lenguaje java son números
