0d1eb0d8bda8487ee62e731c294e8128d92e3918
SenyuMei <SenYuMei@hotmail.com>
2025-02-13 11:03:33 +0100

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,32 @@
+// para almacena variables se empieza por 'let' y luego nombre_variable = "valor";
+
+let texto1 = "Hola mundo";
+console.log(texto1);
+let numero1 = 420;
+console.log(numero1);
+
+// importante no poner comillas porque sino imprimira "texto" y no la variable de texto
+//otra manera de declarar variables es con var
+
+var texto2 = "Hello world";
+console.log(texto2);
+var numero2 = 024;
+console.log(numero2);
+
+//se le puede cambiar el valor a uno ya establecido
+
+let cambio = "Texto normal";
+console.log(cambio);
+cambio = "Texto cambiado";
+console.log(cambio);
+
+//asignar la variable de uno a otro
+
+let nombre = "Senyu";
+let apellido = "Mei";
+
+nombre = apellido;
+
+console.log(nombre);
+
+// output: Mei
