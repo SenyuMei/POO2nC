efd39a398f271c123f20acde0771ff1550a73eb6
SenyuMei <SenYuMei@hotmail.com>
2025-02-13 10:40:17 +0100

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,6 @@
+console.log('Eesto es un string');
+console.log("Esto es un string");
+
+// se puede escribir tanto comillas como doble comillas
+
+console.log("En Java no es necesario acabar con punto y coma como en este ejmplo")
